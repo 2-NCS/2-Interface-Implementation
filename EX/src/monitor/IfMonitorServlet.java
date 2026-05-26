17d445a30e2c9cb0536d94276e29c56f0d25091b
kinetas <kimjh7302@naver.com>
2026-05-26 15:36:27 +0900

    init을 통한 오브젝트 생성 후 멤버변수에 저장

--- EX/src/monitor/IfMonitorServlet.java
+++ EX/src/monitor/IfMonitorServlet.java
@@ -21,7 +21,7 @@ public class IfMonitorServlet extends HttpServlet {
     private AccountDAO accountDAO;
 
     @Override
-    public void init() {
+    public void init() {// init을 통한 오브젝트 생성 후 멤버변수에 저장
         outboxDAO  = new OutboxDAO();
         inboxDAO   = new InboxDAO();
         accountDAO = new AccountDAO();
