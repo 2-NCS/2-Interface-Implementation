a747895e03cd0adc7530ec8212b9550b18a34f25
byeongseon <byeongseon8@gmail.com>
2026-05-27 10:19:11 +0900

    SHA-256 암호화 알고리즘 객체 생성

--- EX/src/common/HashUtil.java
+++ EX/src/common/HashUtil.java
@@ -28,6 +28,7 @@ public class HashUtil {
     public static String sha256(String plain) {
         if (plain == null) return null;
         try {
+        	// SHA-256 암호화 알고리즘 객체 생성
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] hash = md.digest(plain.getBytes(StandardCharsets.UTF_8));
             StringBuilder sb = new StringBuilder(64);
